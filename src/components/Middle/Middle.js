30687e9c4ee78bde4fb188e96449a417806f09fa
dakshmalhotra <dakshmalhotra@hotmail.com>
2019-01-12 16:27:21 -0500

    first commit

---
+++ src/components/Middle/Middle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import './Middle.css';
+
+class Middle extends Component {
+  render() {
+    return (
+        <div className="middle">
+            <img src={logo} className="App-logo" alt="logo" />
+				</div>
+    );
+  }
+}
+
+export default Middle;
